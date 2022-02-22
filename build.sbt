1fa2fb08fd5bfec5a0134c2522c73251997033b2
Konstantyn <sinkkostia@gmail.com>
2022-02-22 20:53:18 +0200

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "Scala3fromScratch"
+
+version := "0.1"
+
+scalaVersion := "3.1.0"
---
+++ src/main/scala/Test.scala
@@ -0,0 +1,22 @@
+import scala.math.sqrt
+
+object Test extends App {
+
+ // x^2 + x - 2 = 0
+//
+// val a: Int = 1
+// val b: Int = 1
+// val c: Int = -2
+//
+// def findRoots( a: Int, b: Int, c: Int): (Double,Double) = {
+//   val d = b*b - 4 * a * c
+//   val x1 = -b + sqrt(d) / 2 * a
+//   val x2 = -b - sqrt(d) / 2 * a
+//   (x1, x2)
+// }
+//
+//  print(findRoots(a , b , c))
+
+print("Hello world!")
+
+}
